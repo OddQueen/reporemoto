ad13a0f3f102027e736cf141ca87bf2a2b881238
OddQueen <queensleymei@gmail.com>
2024-03-19 22:26:49 +0100

    añadido archivo funciones

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const suma = (op1, op2) => op1 + op2;
+const resta = (op1, op2) => op1 - op2;
+const producto = (op1, op2) => op1 * op2;
+const division = (op1, op2) => op1 / op2;
+const cuadrado = (num) => num ** 2;
+
+module.exports = { suma, resta, producto, division, cuadrado };
+
---
+++ run.js
@@ -0,0 +1,8 @@
+
+let myLib = require("./index");
+
+console.log("Suma:", myLib.suma(2, 3));
+console.log("Resta:", myLib.resta(5, 3));
+console.log("Producto:", myLib.producto(4, 6));
+console.log("División:", myLib.division(10, 2));
+console.log("Cuadrado:", myLib.cuadrado(4));
