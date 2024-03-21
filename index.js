f550845572f69db1951dd0d5e61327663e70b4eb
OddQueen <queensleymei@gmail.com>
2024-03-21 23:16:07 +0100

    modificada funcion suma para recibir 3 parametros

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-const suma = (op1, op2) => op1 + op2;
+const suma = (op1, op2, op3) => op1 + op2 + op3;
 const resta = (op1, op2) => op1 - op2;
 const producto = (op1, op2) => op1 * op2;
 const division = (op1, op2) => op1 / op2;
